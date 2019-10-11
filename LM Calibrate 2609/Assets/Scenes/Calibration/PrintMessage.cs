508d4f86a5c8afa6f985128ea34b9717a3c9ba1c
nurulaimanabdr <nurulaimanabdr@users.noreply.github.com>
2019-10-11 17:02:33 +0800

    add Finger Extended

---
+++ LM Calibrate 2609/Assets/Scenes/Calibration/PrintMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintMessage : MonoBehaviour
+{
+    public void PrintActivateMessage ()
+    {
+        Debug.Log ("Activated");
+    }
+
+    public void PrintDeactivateMessage ()
+    {
+        Debug.Log ("Deactivated");
+    }
+}
